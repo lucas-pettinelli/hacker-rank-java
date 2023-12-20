0c7a5df1bd47cb6aa8ccbd5fcfa42b432fa65657
Lucas William <152342034+lucas-pettinelli@users.noreply.github.com>
2023-12-19 22:34:40 -0300

    Create javaStringsIntroduction.java

---
+++ javaStringsIntroduction.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int a =A.length()+B.length();
+        System.out.println(a);
+        int b =A.compareTo(B);
+        if(b>0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }        
+        System.out.println(A.substring(0,1).toUpperCase()+A.substring(1)+" "+B.substring(0,1).toUpperCase()+B.substring(1));
+    }
+}
+
